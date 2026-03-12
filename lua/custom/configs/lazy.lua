e7112620b8d1e86bebb108c6abd7136590c03bd6
DenerThiago21 <denerthi.ibi@gmail.com>
2026-03-12 15:42:26 -0300

    add lazy-lock.json

--- lua/custom/configs/lazy.lua
+++ lua/custom/configs/lazy.lua
@@ -1,6 +1,6 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
@@ -16,6 +16,8 @@ require('lazy').setup({
         -- import yout plugins
         { import = "custom.plugins" },
     },
+    lockfile = vim.fn.stdpath("config") .."/lazy-lock.json",
+    install = { missing = false }
 }, { ---@diagnostic disable-line: missing-fields
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
