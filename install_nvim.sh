ed2608ce15999a10c84d607b22b71cf8886af5b9
DenerThiago21 <denerthi.ibi@gmail.com>
2026-03-12 20:43:28 -0300

    change install_nvim.sh to donwload neovim.appimage

--- install_nvim.sh
+++ install_nvim.sh
@@ -2,11 +2,14 @@
 
 echo "Installing Neovim ..."
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim-linux-x86_64
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+./nvim-linux-x86_64.appimage
 
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+mkdir -p /opt/nvim
+mv nvim-linux-x86_64.appimage /opt/nvim/nvim
+
+export PATH="$PATH:/opt/nvim/"
 
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
