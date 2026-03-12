e4b71b0b02cf74e5930feb937032417d3b88a66e
DenerThiago21 <denerthi.ibi@gmail.com>
2026-03-12 11:56:10 -0300

    add files neo-tree.lua and web-dev-icons.lua plugins

---
+++ lua/custom/plugins/nvim-web-devicons.lua
@@ -0,0 +1,17 @@
+-- Example configuration for nvim-web-devicons
+return {
+  'nvim-tree/nvim-web-devicons',
+  opts = {
+    -- Otions go here if you need
+  },
+  lazy = false,
+}
+
+-- Example configuration for mini.icons
+-- return
+-- {
+-- 	"echasnovski/mini.icons",
+-- 	opts = {
+-- 		-- Options go here if needed
+-- 	},
+-- }
