a49732d1fa04510d1d1b8fcc56df1dfb022361b8
DenerThiago21 <denerthi.ibi@gmail.com>
2026-03-13 15:03:22 -0300

    adjusts

--- lua/custom/configs/lazy.lua
+++ lua/custom/configs/lazy.lua
@@ -1,6 +1,6 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath("data") .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
@@ -11,34 +11,14 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require('lazy').setup({
-    spec= {
-        -- import yout plugins
-        { import = "custom.plugins" },
-    },
-    lockfile = vim.fn.stdpath("config") .."/lazy-lock.json",
-    install = { missing = false }
-}, { ---@diagnostic disable-line: missing-fields
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
+require('lazy').setup {
+  spec = {
+    -- import yout plugins
+    { import = 'custom.plugins' },
   },
-})
+  lockfile = vim.fn.stdpath 'config' .. '/lazy-lock.json',
+  install = { missing = false },
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
